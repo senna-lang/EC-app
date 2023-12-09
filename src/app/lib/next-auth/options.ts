6c5fff9362017fa70f27ddf9dd9c927048d4f85b
senna-lang <asrsena3302@gmail.com>
2023-12-09 12:27:52 +0900

    nextAuth init

--- src/app/lib/next-auth/options.ts
+++ src/app/lib/next-auth/options.ts
@@ -1,14 +1,14 @@
-import { NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '../prisma';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GitHubProvider from "next-auth/providers/github";
+import { NextAuthOptions } from "next-auth";
+import prisma from "../prisma";
 
 export const nextAuthOptions: NextAuthOptions = {
-  debug: false,
+  debug: true,
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
   adapter: PrismaAdapter(prisma),
@@ -23,4 +23,5 @@ export const nextAuthOptions: NextAuthOptions = {
       };
     },
   },
+  secret: process.env.NEXTAUTH_SECRET,
 };
