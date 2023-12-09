534a98c601b7d67e5ed6ee8ff7afd50d04549bfc
senna-lang <asrsena3302@gmail.com>
2023-12-09 12:28:22 +0900

    nextAuthのsessionProvider実装

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,16 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { useSession } from 'next-auth/react';
 
 const Header = () => {
+  const { data: session } = useSession();
+  const user = session?.user;
   return (
     <header className="bg-slate-600 text-gray-100 shadow-lg">
       <nav className="flex items-center justify-between p-4">
-        <Link href={"/"} className="text-xl font-bold">
+        <Link href={'/'} className="text-xl font-bold">
           Book Commerce
         </Link>
         <div className="flex items-center gap-1">
@@ -18,18 +21,28 @@ const Header = () => {
             ホーム
           </Link>
           <Link
-            href="/login"
+            href={user ? '/profile' : '/api/auth/signin'}
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
-            ログイン
+            {user ? 'プロフィール' : 'ログイン'}
           </Link>
+          {user ? (
+            <Link
+              href={'/api/auth/signout?callbackUrl=/'}
+              className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              ログアウト
+            </Link>
+          ) : (
+            ''
+          )}
 
           <Link href={`/profile`}>
             <Image
               width={50}
               height={50}
               alt="profile_icon"
-              src={"/default_icon.png"}
+              src={user?.image || '/default_icon.png'}
             />
           </Link>
         </div>
