fe6b6ec256e12de85dbc7821a72da3a6cd598a2e
senna-lang <asrsena3302@gmail.com>
2023-12-11 21:36:53 +0900

    strict mode false

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   images: {
-      remotePatterns : [
-         {
-            protocol : "https",
-            hostname : "avatars.githubusercontent.com",
-         },
-         {
-            protocol : "https",
-            hostname : "images.microcms-assets.io",
-         }
-      ]
-   }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+      },
+    ],
+  },
+  reactStrictMode: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
