ccff7612c7a9addc859ce0b95bda19e9e0129b2c
senna-lang <asrsena3302@gmail.com>
2023-12-12 21:50:54 +0900

    小さな修正

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,16 +1,16 @@
 import { getDetailBook } from '@/app/lib/microcms/client';
 import Image from 'next/image';
-import React from 'react';
 
 const DetailBook = async ({ params }: { params: { id: string } }) => {
   const book = await getDetailBook(params.id);
+
   return (
     <div className="container mx-auto p-4">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <Image
+          className="w-full h-80 object-cover object-center"
           src={book.thumbnail.url}
           alt={book.title}
-          className="w-full h-80 object-cover object-center"
           width={700}
           height={700}
         />
@@ -22,8 +22,12 @@ const DetailBook = async ({ params }: { params: { id: string } }) => {
           />
 
           <div className="flex justify-between items-center mt-2">
-            <span className="text-sm text-gray-500">公開日:{new Date(book.createdAt).toLocaleDateString()}</span>
-            <span className="text-sm text-gray-500">最終更新:{new Date(book.updatedAt).toLocaleDateString()}</span>
+            <span className="text-sm text-gray-500">
+              公開日: {new Date(book.createdAt).toLocaleString()}
+            </span>
+            <span className="text-sm text-gray-500">
+              最終更新: {new Date(book.updatedAt).toLocaleString()}
+            </span>
           </div>
         </div>
       </div>
